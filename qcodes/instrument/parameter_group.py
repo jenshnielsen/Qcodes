b9930ccdc4c7d175f1497608b4cc36fb5232c05c
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2018-06-06 13:36:47 +0200

    Make it actually work even if still limited

--- qcodes/instrument/parameter_group.py
+++ qcodes/instrument/parameter_group.py
@@ -8,6 +8,13 @@ class ParameterGroup:
 
     def __init__(self, *parameters: Parameter) -> None:
         self.__parameters = parameters
+        self.__parameter_dict = {}
+
+        for parameter in parameters:
+            self.__parameter_dict[parameter.name] = parameter
+
+        for parameter in self.__parameters:
+            assert parameter is self.__parameter_dict[parameter.name]
 
     def get(self) -> Dict['str', Union[dict, value_types]]:
         captured_values = {}
@@ -20,3 +27,9 @@ class ParameterGroup:
 
         for param_name, param_value in values.items():
             getattr(self, param_name).set(param_value)
+
+    def __getattr__(self, name: str) -> Parameter:
+        if name in self.__parameter_dict.keys():
+            return self.__parameter_dict[name]
+
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
--- qcodes/tests/test_paramter_group.py
+++ qcodes/tests/test_paramter_group.py
@@ -24,5 +24,9 @@ def test_parameter_group_basic(some_dummy_parameters):
     expected_values = {'a': 0, 'b': 1, 'c': 2}
     set_values = {'a': 21, 'b': 11, 'c': 33}
     assert pg.get() == expected_values
+    for parameter_name, expected_value in expected_values.items():
+        assert getattr(pg, parameter_name).get() == expected_value
     pg.set(set_values)
     assert pg.get() == set_values
+    for parameter_name, expected_value in set_values.items():
+        assert getattr(pg, parameter_name).get() == expected_value
