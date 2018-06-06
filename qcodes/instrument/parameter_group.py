d0ae523b45bbdd7f363c3dab33607010539b8a15
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2018-06-06 17:05:34 +0200

    TODO

--- qcodes/instrument/parameter_group.py
+++ qcodes/instrument/parameter_group.py
@@ -5,16 +5,22 @@ value_types = Union[int, float, str]
 
 
 class ParameterGroup:
+    """
 
-    def __init__(self, *parameters: Parameter) -> None:
+
+    TODO: It would be handy to be able to pick out a single value from a nested
+    level for cases where this is in all outer levers
+    TODO: This still needs docstrings, handling of dependencies as in array
+    parameters. and correct snapshot handling.
+
+    """
+    def __init__(self, name: str, *parameters: Union[Parameter,
+                                                     'ParameterGroup']) -> None:
         self.__parameters = parameters
         self.__parameter_dict = {}
-
+        self.__name = name
         for parameter in parameters:
             self.__parameter_dict[parameter.name] = parameter
-
-        for parameter in self.__parameters:
-            assert parameter is self.__parameter_dict[parameter.name]
 
     def get(self) -> Dict['str', Union[dict, value_types]]:
         captured_values = {}
@@ -22,6 +28,10 @@ class ParameterGroup:
         for parameter in self.__parameters:
             captured_values[parameter.name] = parameter.get()
         return captured_values
+
+    @property
+    def name(self):
+        return self.__name
 
     def set(self, values: Dict[str, Union[dict, value_types]]):
 
@@ -32,4 +42,6 @@ class ParameterGroup:
         if name in self.__parameter_dict.keys():
             return self.__parameter_dict[name]
 
-        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+        raise AttributeError(f"'{self.__class__.__name__}' object has"
+                             f" no attribute '{name}'")
+
--- qcodes/tests/test_paramter_group.py
+++ qcodes/tests/test_paramter_group.py
@@ -17,12 +17,25 @@ def some_dummy_parameters() -> List[Parameter]:
                                 set_cmd=None, get_cmd=None))
     return dummy_parameters
 
+@fixture
+def more_dummy_parameters() -> List[Parameter]:
+
+    dummy_parameters = []
+    for name, value in zip(['d', 'e', 'f'], range(3,6)):
+        dummy_parameters.append(Parameter(name=name,
+                                label=name.upper(),
+                                initial_value=value,
+                                set_cmd=None, get_cmd=None))
+    return dummy_parameters
+
 
 def test_parameter_group_basic(some_dummy_parameters):
 
-    pg = ParameterGroup(*some_dummy_parameters)
+    pg = ParameterGroup('mypg', *some_dummy_parameters)
     expected_values = {'a': 0, 'b': 1, 'c': 2}
     set_values = {'a': 21, 'b': 11, 'c': 33}
+
+    assert pg.name == 'mypg'
     assert pg.get() == expected_values
     for parameter_name, expected_value in expected_values.items():
         assert getattr(pg, parameter_name).get() == expected_value
@@ -30,3 +43,18 @@ def test_parameter_group_basic(some_dummy_parameters):
     assert pg.get() == set_values
     for parameter_name, expected_value in set_values.items():
         assert getattr(pg, parameter_name).get() == expected_value
+
+
+def test_nested_parameter_group(some_dummy_parameters,
+                                more_dummy_parameters):
+
+    pg1 = ParameterGroup('pg1', *some_dummy_parameters)
+    pg2 = ParameterGroup('pg2', *more_dummy_parameters)
+
+    outerpg = ParameterGroup('outerpg', pg1, pg2)
+
+    expected_values1 = {'a': 0, 'b': 1, 'c': 2}
+    expected_values2 = {'d': 3, 'e': 4, 'f': 5}
+    expected_values = {'pg1': expected_values1,
+                       'pg2': expected_values2}
+    assert outerpg.get() == expected_values
