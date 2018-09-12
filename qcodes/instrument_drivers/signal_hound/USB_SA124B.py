97d51f06bcd04d67041f66e3ddd6ee1e852e8b3a
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2018-09-12 09:38:50 +0200

    Rewrite signalhound
    Use new array parameter, keep track of if config has been changed but not yet synced,

--- qcodes/instrument_drivers/signal_hound/USB_SA124B.py
+++ qcodes/instrument_drivers/signal_hound/USB_SA124B.py
@@ -4,7 +4,11 @@ import numpy as np
 import ctypes as ct
 import logging
 
-from qcodes import Instrument, validators as vals
+from qcodes.instrument.base import Instrument
+import qcodes.utils.validators as vals
+from qcodes.instrument.parameter import ArrayParameter2, Parameter
+
+
 from enum import IntEnum
 log = logging.getLogger(__name__)
 
@@ -19,52 +23,66 @@ class SignalHound_USB_SA124B(Instrument):
         t0 = time()
         super().__init__(name, **kwargs)
 
-        log.info('Initializing instrument SignalHound USB 124A')
+        log.info('Initializing instrument SignalHound USB 124B')
         self.dll = ct.CDLL(dll_path or self.dll_path)
         self.hf = constants
+
+        self.config_synced = False
+        self.initialisation_synced = False
 
         self.add_parameter('frequency',
                            label='Frequency ',
                            unit='Hz',
                            initial_value=5e9,
-                           get_cmd=None, set_cmd=None,
+                           get_cmd=None,
+                           parameter_class=TraceParameter,
                            vals=vals.Numbers())
         self.add_parameter('span',
                            label='Span ',
                            unit='Hz',
                            initial_value=.25e6,
-                           get_cmd=None, set_cmd=None,
+                           get_cmd=None,
+                           parameter_class=TraceParameter,
                            vals=vals.Numbers())
-        self.add_parameter('power',
-                           label='Power ',
-                           unit='dBm',
-                           initial_value=0,
-                           get_cmd=None, set_cmd=None,
-                           vals=vals.Numbers(max_value=20))
         self.add_parameter('ref_lvl',
                            label='Reference power ',
                            unit='dBm',
                            initial_value=0,
-                           get_cmd=None, set_cmd=None,
+                           get_cmd=None,
+                           parameter_class=TraceParameter,
                            vals=vals.Numbers(max_value=20))
         self.add_parameter('external_reference',
                            get_cmd=None, set_cmd=None,
                            initial_value=False,
+                           parameter_class=TraceParameter,
                            vals=vals.Bool())
         self.add_parameter('device_type',
                            get_cmd=self._do_get_device_type)
 
         self.add_parameter('device_mode',
                            initial_value='sweeping',
-                           get_cmd=None, set_cmd=None,
+                           get_cmd=None,
+                           parameter_class=TraceParameter,
+                           update_function='initialisation_synced',
                            vals=vals.Anything())
-        self.add_parameter('acquisition_mode',
-                           get_cmd=None, set_cmd=None,
-                           initial_value='average',
-                           vals=vals.Enum('average', 'min-max'))
+
+        self.add_parameter('n_avg',
+                           get_cmd=None,
+                           set_cmd=None,
+                           initial_value=1,
+                           vals=vals.Ints(1))
+
+        # not implemented, not sure how it is useful.
+        # was broken in the old driver anyway
+        # self.add_parameter('acquisition_mode',
+        #                    get_cmd=None,
+        #                    initial_value='average',
+        #                    parameter_class=TraceParameter,
+        #                    vals=vals.Enum('average', 'min-max'))
         self.add_parameter('scale',
-                           get_cmd=None, set_cmd=None,
+                           get_cmd=None,
                            initial_value='log-scale',
+                           parameter_class=TraceParameter,
                            vals=vals.Enum('log-scale', 'lin-scale',
                                           'log-full-scale', 'lin-full-scale'))
         self.add_parameter('running',
@@ -72,21 +90,26 @@ class SignalHound_USB_SA124B(Instrument):
                            initial_value=False,
                            vals=vals.Bool())
         self.add_parameter('decimation',
-                           get_cmd=None, set_cmd=None,
+                           get_cmd=None,
                            initial_value=1,
+                           parameter_class=TraceParameter,
+                           docstring='Only used in IQ mode',
                            vals=vals.Ints(1, 8))
         self.add_parameter('bandwidth',
                            label='Bandwidth',
                            unit='Hz',
                            initial_value=0,
-                           get_cmd=None, set_cmd=None,
+                           parameter_class=TraceParameter,
+                           get_cmd=None,
+                           docstring='Only used in IQ mode',
                            vals=vals.Numbers())
         # rbw Resolution bandwidth in Hz. RBW can be arbitrary.
         self.add_parameter('rbw',
                            label='Resolution Bandwidth',
                            unit='Hz',
                            initial_value=1e3,
-                           get_cmd=None, set_cmd=None,
+                           parameter_class=TraceParameter,
+                           get_cmd=None,
                            vals=vals.Numbers())
         # vbw Video bandwidth in Hz. VBW must be less than or equal to RBW.
         #  VBW can be arbitrary. For best performance use RBW as the VBW.
@@ -94,8 +117,36 @@ class SignalHound_USB_SA124B(Instrument):
                            label='Video Bandwidth',
                            unit='Hz',
                            initial_value=1e3,
-                           get_cmd=None, set_cmd=None,
+                           parameter_class=TraceParameter,
+                           get_cmd=None,
                            vals=vals.Numbers())
+
+        self.add_parameter('reject_image',
+                           label='Reject image',
+                           unit='',
+                           initial_value=True,
+                           parameter_class=TraceParameter,
+                           get_cmd=None,
+                           vals=vals.Bool())
+
+        self.add_parameter('frequency_axis',
+                           label='Frequency',
+                           unit='Hz',
+                           get_cmd=self._get_freq_axis,
+                           set_cmd=False,
+                           vals=vals.Arrays(),
+                           snapshot_value=False
+                           )
+
+        self.add_parameter('freq_sweep',
+                           label='Power',
+                           unit='depends on mode',
+                           get_cmd=self._get_sweep,
+                           set_cmd=False,
+                           parameter_class=ArrayParameter2,
+                           vals=vals.Arrays(),
+                           setpoints=(self.frequency_axis,),
+                           snapshot_value=False)
 
         self.openDevice()
         self.device_type()
@@ -104,10 +155,6 @@ class SignalHound_USB_SA124B(Instrument):
         # poor-man's connect_message. We could overwrite get_idn
         # instead and use connect_message.
         print('Initialized SignalHound in %.2fs' % (t1-t0))
-
-    @classmethod
-    def default_server_name(cls, **kwargs):
-        return 'USB'
 
     def openDevice(self):
         log.info('Opening Device')
@@ -119,22 +166,35 @@ class SignalHound_USB_SA124B(Instrument):
                              f'{saStatus(ret).name}')
 
         self.devOpen = True
-        self.get('device_type')
 
-    def close(self):
+    def close(self, preset=False):
+        """
+
+        Args:
+            preset: If True performs a hardware reset of the device.
+
+        """
         log.info('Closing Device with handle num: '
                  f'{self.deviceHandle.value}')
-
-        try:
-            self.dll.saAbort(self.deviceHandle)
-            log.info('Running acquistion aborted.')
-        except Exception as e:
-            log.info(f'Could not abort acquisition: {e}')
-
-        ret = self.dll.saCloseDevice(self.deviceHandle)
-        if ret != saStatus.saNoError:
-            raise ValueError('Error closing device!')
-        log.info(f'Closed Device with handle num: {self.deviceHandle.value}')
+        if preset is True:
+            log.warning('Performing hardware-reset of device!')
+            ret = self.dll.saPreset(self.deviceHandle)
+            if ret != saStatus.saNoError:
+                log.warning('Error performing preset of device: '
+                            f'{saStatus(ret).name}')
+            log.info(
+                f'Preset Device with handle num: {self.deviceHandle.value}')
+        else:
+            ret = self.dll.saAbort(self.deviceHandle)
+            if ret != saStatus.saNoError:
+                raise ValueError('Error performing abort: '
+                                 f'{saStatus(ret).name}')
+            ret = self.dll.saCloseDevice(self.deviceHandle)
+            if ret != saStatus.saNoError:
+                raise ValueError('Error performing abort: '
+                                 f'{saStatus(ret).name}')
+            log.info(
+                f'Closed Device with handle num: {self.deviceHandle.value}')
         self.devOpen = False
         self.running(False)
         super().close()
@@ -153,19 +213,6 @@ class SignalHound_USB_SA124B(Instrument):
                           'without ever calling initiate()?')
         else:
             raise IOError('Unknown error setting abort! Error = %s' % err)
-
-    def preset(self):
-        log.warning('Performing hardware-reset of device!')
-        log.warning('Please ensure you close the device handle within '
-                    'two seconds of this call!')
-
-        err = self.dll.saPreset(self.deviceHandle)
-        if err == saStatus.saNoError:
-            log.info('Call to preset succeeded.')
-        elif err == saStatus.saDeviceNotOpenErr:
-            raise IOError('Device not open!')
-        else:
-            raise IOError(f'Unknown error calling preset! Error = {err}')
 
     def _do_get_device_type(self):
         log.info('Querying device for model information')
@@ -244,13 +291,16 @@ class SignalHound_USB_SA124B(Instrument):
         elif err == saStatus.saBandwidthErr:
             raise IOError('RBW is larger than your span. (Sweep Mode)!')
         self.check_for_error(err)
-
+        self.initialisation_synced = True
         return
 
     def QuerySweep(self):
         """
         Queries the sweep for information on the parameters it uses
         """
+        if self.config_synced is False or self.initialisation_synced is False:
+            raise RuntimeError("Trying to query sweep settings but parameters "
+                               "have not been synced to device")
         sweep_len = ct.c_int(0)
         start_freq = ct.c_double(0)
         stepsize = ct.c_double(0)
@@ -267,12 +317,20 @@ class SignalHound_USB_SA124B(Instrument):
         elif err == saStatus.saNullPtrErr:
             raise IOError('Null pointer error!')
         else:
-            raise IOError('Unknown error!')
+            raise IOError('Could not query stream got error:'
+                          f'{saStatus(err).name}')
 
         info = [sweep_len.value, start_freq.value, stepsize.value]
         return info
 
-    def configure(self, rejection=True):
+    def _get_freq_axis(self) -> np.ndarray:
+        sweep_len, start_freq, stepsize = self.QuerySweep()
+
+        end_freq = start_freq + stepsize*(sweep_len-1)
+        freq_points = np.linspace(start_freq, end_freq, sweep_len)
+        return freq_points
+
+    def configure(self):
         """
         Configure consists of five parts
             1. Center span configuration (freqs and span)
@@ -311,25 +369,20 @@ class SignalHound_USB_SA124B(Instrument):
             'log-full-scale': ct.c_uint(self.hf.sa_LOG_FULL_SCALE),
             'lin-full-scale': ct.c_uint(self.hf.sa_LIN_FULL_SCALE)
         }
-        if self.acquisition_mode() in detectorVals:
-            detector = detectorVals[self.acquisition_mode()]
-        else:
-            raise ValueError('Invalid Detector mode! Detector  must be one of '
-                             f'{list(detectorVals.keys())}. Specified '
-                             f'detector = {self.acquisition_mode()}')
         if self.scale() in scaleVals:
             scale = scaleVals[self.scale()]
         else:
             raise ValueError('Invalid Scaling mode! Scaling mode must be one '
                              f'of {list(scaleVals.keys())}. '
                              f'Specified scale = {self.scale()}')
-        err = self.dll.saConfigAcquisition(self.deviceHandle, detector, scale)
+        err = self.dll.saConfigAcquisition(self.deviceHandle,
+                                           detectorVals['average'], scale)
         self.check_for_error(err)
 
         # Reference Level configuration
         log.info('Setting device reference level configuration.')
         err = self.dll.saConfigLevel(
-            self.deviceHandle, ct.c_double(self.get('ref_lvl')))
+            self.deviceHandle, ct.c_double(self.ref_lvl()))
         self.check_for_error(err)
 
         # External Reference configuration
@@ -340,7 +393,7 @@ class SignalHound_USB_SA124B(Instrument):
 
         if self.device_mode() == 'sweeping':
             # Sweeping Configuration
-            reject_var = ct.c_bool(rejection)
+            reject_var = ct.c_bool(self.reject_image())
             log.info('Setting device Sweeping configuration.')
             err = self.dll.saConfigSweepCoupling(
                 self.deviceHandle, ct.c_double(self.get('rbw')),
@@ -351,9 +404,10 @@ class SignalHound_USB_SA124B(Instrument):
                 self.deviceHandle, ct.c_int(self.get('decimation')),
                 ct.c_double(self.get('bandwidth')))
             self.check_for_error(err)
-        return
+        # Now we have synced all the parameters to the SA
+        self.config_synced = True
 
-    def sweep(self):
+    def _get_sweep(self):
         """
         This function performs a sweep over the configured ranges.
         The result of the sweep is returned along with the sweep points
@@ -361,52 +415,45 @@ class SignalHound_USB_SA124B(Instrument):
         returns:
 
         """
-        try:
-            sweep_len, start_freq, stepsize = self.QuerySweep()
-        except:
-            self.prepare_for_measurement()
-            sweep_len, start_freq, stepsize = self.QuerySweep()
-
-        end_freq = start_freq + stepsize*(sweep_len-1)
-        freq_points = np.linspace(start_freq, end_freq, sweep_len)
+        if self.device_mode() != 'sweeping':
+            raise RuntimeError("Trying to perform a sweep but "
+                               " instrument is not in sweep mode.")
+        self.prepare_for_measurement()
+        if self.config_synced is False or self.initialisation_synced is False:
+            raise RuntimeError("Trying to capture sweep but parameters "
+                               "have not been synced to device")
+        freq_points = self._get_freq_axis()
+        sweep_len = len(freq_points)
 
         minarr = (ct.c_float * sweep_len)()
         maxarr = (ct.c_float * sweep_len)()
-        sleep(.1)  # Added extra sleep for updating issue
-        err = self.dll.saGetSweep_32f(self.deviceHandle, minarr, maxarr)
-        sleep(.1)  # Added extra sleep
-        if not err == saStatus.saNoError:
-            # if an error occurs tries preparing the device and then asks again
-            print('Error raised in QuerySweepInfo, preparing for measurement')
-            sleep(.1)
-            self.prepare_for_measurement()
-            sleep(.1)
-            minarr = (ct.c_float * sweep_len)()
-            maxarr = (ct.c_float * sweep_len)()
+        n_avg = self.n_avg.get()
+        datamin = np.zeros(sweep_len, dtype=np.float64)
+        for i in range(n_avg):
+            sleep(.1)  # Added extra sleep for updating issue
             err = self.dll.saGetSweep_32f(self.deviceHandle, minarr, maxarr)
 
-        if err == saStatus.saNoError:
-            pass
-        elif err == saStatus.saDeviceNotOpenErr:
-            raise IOError('Device not open!')
-        elif err == saStatus.saDeviceNotConfiguredErr:
-            raise IOError('The device specified is not currently streaming!')
-        elif err == saStatus.saNullPtrErr:
-            raise IOError('Null pointer error!')
-        elif err == saStatus.saInvalidModeErr:
-            raise IOError('Invalid mode error!')
-        elif err == saStatus.saCompressionWarning:
-            raise IOError('Input voltage overload!')
-        elif err == saStatus.sCUSBCommErr:
-            raise IOError('Error ocurred in the USB connection!')
-        else:
-            raise IOError('Unknown error!')
+            if err == saStatus.saNoError:
+                pass
+            elif err == saStatus.saDeviceNotOpenErr:
+                raise IOError('Device not open!')
+            elif err == saStatus.saDeviceNotConfiguredErr:
+                raise IOError('The device specified is not currently streaming!')
+            elif err == saStatus.saNullPtrErr:
+                raise IOError('Null pointer error!')
+            elif err == saStatus.saInvalidModeErr:
+                raise IOError('Invalid mode error!')
+            elif err == saStatus.saCompressionWarning:
+                raise IOError('Input voltage overload!')
+            elif err == saStatus.sCUSBCommErr:
+                raise IOError('Error occurred in the USB connection!')
+            else:
+                raise IOError('Unknown error!')
 
-        # note if used in averaged mode (set in config) datamin=datamax
-        datamin = np.array([minarr[elem] for elem in range(sweep_len)])
-        datamax = np.array([maxarr[elem] for elem in range(sweep_len)])
-
-        return np.array([freq_points, datamin, datamax])
+            # note if used in averaged mode, the only mode currently supported
+            # minarr=maxarr
+            datamin += np.array([minarr[elem] for elem in range(sweep_len)])
+        return datamin / n_avg
 
     def get_power_at_freq(self, Navg=1):
         """
@@ -437,14 +484,9 @@ class SignalHound_USB_SA124B(Instrument):
         return np.array([sweep_points, data_spec])
 
     def prepare_for_measurement(self):
-        self.set('device_mode', 'sweeping')
         self.configure()
         self.initialisation()
         return
-
-    def safe_reload(self):
-        self.closeDevice()
-        self.reload()
 
     def check_for_error(self, err):
         if err != saStatus.saNoError:
@@ -572,3 +614,20 @@ class saStatus(IntEnum):
     saCompressionWarning = 2
     saParameterClamped = 3
     saBandwidthClamped = 4
+
+
+class TraceParameter(Parameter):
+    """
+    A parameter that keeps track of if its value has been synced to
+    the USB device by calling configure or similar function.
+    """
+    def __init__(self, *args, update_function='config_synced', **kwargs):
+        if update_function is None:
+            raise RuntimeError("You must supply an update function to"
+                               "the Tracing parameter")
+        self._update_function = update_function
+        super().__init__(*args, **kwargs)
+
+    def set_raw(self, value):
+        setattr(self.instrument, self._update_function, False)
+        self._save_val(value, validate=False)
