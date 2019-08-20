db86dff8c394fc9ef8e62a13426abc3fc9b9a800
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2019-08-20 20:52:03 +0200

    remove all toplevel imports from qcodes

--- qcodes/__init__.py
+++ qcodes/__init__.py
@@ -13,77 +13,80 @@ from .version import __version__
 add_to_spyder_UMR_excludelist('qcodes')
 config: Config = Config()
 
-from qcodes.version import __version__
 
-plotlib = config.gui.plotlib
-if plotlib in {'QT', 'all'}:
+# todo make this a config with a warning
+legacy_import = False
+
+if legacy_import:
+
+    plotlib = config.gui.plotlib
+    if plotlib in {'QT', 'all'}:
+        try:
+            from qcodes.plots.pyqtgraph import QtPlot
+        except Exception:
+            print('pyqtgraph plotting not supported, '
+                  'try "from qcodes.plots.pyqtgraph import QtPlot" '
+                  'to see the full error')
+
+    if plotlib in {'matplotlib', 'all'}:
+        try:
+            from qcodes.plots.qcmatplotlib import MatPlot
+        except Exception:
+            print('matplotlib plotting not supported, '
+                  'try "from qcodes.plots.qcmatplotlib import MatPlot" '
+                  'to see the full error')
+
+    from qcodes.station import Station
+    from qcodes.loops import Loop, active_loop, active_data_set
+    from qcodes.measure import Measure
+    from qcodes.actions import Task, Wait, BreakIf
+    haswebsockets = True
     try:
-        from qcodes.plots.pyqtgraph import QtPlot
-    except Exception:
-        print('pyqtgraph plotting not supported, '
-              'try "from qcodes.plots.pyqtgraph import QtPlot" '
-              'to see the full error')
+        import websockets
+    except ImportError:
+        haswebsockets = False
+    if haswebsockets:
+        from qcodes.monitor.monitor import Monitor
 
-if plotlib in {'matplotlib', 'all'}:
-    try:
-        from qcodes.plots.qcmatplotlib import MatPlot
-    except Exception:
-        print('matplotlib plotting not supported, '
-              'try "from qcodes.plots.qcmatplotlib import MatPlot" '
-              'to see the full error')
+    from qcodes.data.data_set import DataSet, new_data, load_data
+    from qcodes.data.location import FormatLocation
+    from qcodes.data.data_array import DataArray
+    from qcodes.data.format import Formatter
+    from qcodes.data.gnuplot_format import GNUPlotFormat
+    from qcodes.data.hdf5_format import HDF5Format
+    from qcodes.data.io import DiskIO
 
+    from qcodes.instrument.base import Instrument, find_or_create_instrument
+    from qcodes.instrument.ip import IPInstrument
+    from qcodes.instrument.visa import VisaInstrument
+    from qcodes.instrument.channel import InstrumentChannel, ChannelList
 
-from qcodes.station import Station
-from qcodes.loops import Loop, active_loop, active_data_set
-from qcodes.measure import Measure
-from qcodes.actions import Task, Wait, BreakIf
-haswebsockets = True
-try:
-    import websockets
-except ImportError:
-    haswebsockets = False
-if haswebsockets:
-    from qcodes.monitor.monitor import Monitor
+    from qcodes.instrument.function import Function
+    from qcodes.instrument.parameter import (
+        Parameter,
+        ArrayParameter,
+        MultiParameter,
+        ParameterWithSetpoints,
+        DelegateParameter,
+        StandardParameter,
+        ManualParameter,
+        ScaledParameter,
+        combine,
+        CombinedParameter)
+    from qcodes.instrument.sweep_values import SweepFixedValues, SweepValues
 
-from qcodes.data.data_set import DataSet, new_data, load_data
-from qcodes.data.location import FormatLocation
-from qcodes.data.data_array import DataArray
-from qcodes.data.format import Formatter
-from qcodes.data.gnuplot_format import GNUPlotFormat
-from qcodes.data.hdf5_format import HDF5Format
-from qcodes.data.io import DiskIO
+    from qcodes.utils import validators
+    from qcodes.utils.zmq_helpers import Publisher
+    from qcodes.instrument_drivers.test import test_instruments, test_instrument
 
-from qcodes.instrument.base import Instrument, find_or_create_instrument
-from qcodes.instrument.ip import IPInstrument
-from qcodes.instrument.visa import VisaInstrument
-from qcodes.instrument.channel import InstrumentChannel, ChannelList
-
-from qcodes.instrument.function import Function
-from qcodes.instrument.parameter import (
-    Parameter,
-    ArrayParameter,
-    MultiParameter,
-    ParameterWithSetpoints,
-    DelegateParameter,
-    StandardParameter,
-    ManualParameter,
-    ScaledParameter,
-    combine,
-    CombinedParameter)
-from qcodes.instrument.sweep_values import SweepFixedValues, SweepValues
-
-from qcodes.utils import validators
-from qcodes.utils.zmq_helpers import Publisher
-from qcodes.instrument_drivers.test import test_instruments, test_instrument
-
-from qcodes.dataset.measurements import Measurement
-from qcodes.dataset.data_set import new_data_set, load_by_counter, load_by_id, load_by_run_spec, load_by_guid
-from qcodes.dataset.experiment_container import new_experiment, load_experiment, load_experiment_by_name, \
-    load_last_experiment, experiments, load_or_create_experiment
-from qcodes.dataset.sqlite.settings import SQLiteSettings
-from qcodes.dataset.descriptions.param_spec import ParamSpec
-from qcodes.dataset.sqlite.database import initialise_database, \
-    initialise_or_create_database_at
+    from qcodes.dataset.measurements import Measurement
+    from qcodes.dataset.data_set import new_data_set, load_by_counter, load_by_id, load_by_run_spec, load_by_guid
+    from qcodes.dataset.experiment_container import new_experiment, load_experiment, load_experiment_by_name, \
+        load_last_experiment, experiments, load_or_create_experiment
+    from qcodes.dataset.sqlite.settings import SQLiteSettings
+    from qcodes.dataset.descriptions.param_spec import ParamSpec
+    from qcodes.dataset.sqlite.database import initialise_database, \
+        initialise_or_create_database_at
 
 try:
     get_ipython() # type: ignore # Check if we are in iPython
