146a67b576ed441df6ab2cfa907c453637ff7ab3
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2019-09-26 14:01:36 +0200

    more experimenting and some notes

--- qcodes/tests/deltesting.py
+++ qcodes/tests/deltesting.py
@@ -31,6 +31,18 @@ def test_double_kill():
 
 
 def test_single_kill():
+
+    # ctrl_C_event and ctrl_break_events are the only ones that can be send on windows. They can only be send
+    # to a process group and not an individual process. We therefore create a new process group below.
+    # Furthermore they can only be sent to a process bound to the same console not to detached processes or
+    # across console boundaries.
+    # However, new process groups disable handling of ctrl_c events. ctrl-break events are not handled by
+    # the default signal handler for sigint (keyboardinterupt) but trigger a SIGBREAK.
+    # Provided a signal handler is added for sigbreak this test works as expected. But does not test what we want
+    # https://docs.microsoft.com/en-us/windows/win32/procthread/process-creation-flags
+    # https://stackoverflow.com/questions/44124338/trying-to-implement-signal-ctrl-c-event-in-python3-6
+    # it may be possible to restore the python signal handlers here
+    # for setconsolectrlhandler https://docs.microsoft.com/en-us/windows/console/setconsolectrlhandler
     proc = subprocess.Popen("python nointerupt.py",
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
@@ -40,7 +52,10 @@ def test_single_kill():
     while pid is None:
         pid = proc.pid
     print(pid)
-    proc.send_signal(signal.CTRL_BREAK_EVENT)
+    #os.kill(pid, signal.CTRL_BREAK_EVENT)
+    time.sleep(1)
+    proc.send_signal(signal.CTRL_C_EVENT)
+    #proc.send_signal(signal.CTRL_BREAK_EVENT)
     output = proc.communicate()
     print(output)
     # stdout = output[0].splitlines()[-1]
@@ -52,9 +67,7 @@ def test_no_kill():
     proc = subprocess.Popen("python nointerupt.py", shell=True,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
-    signal.signal(signal.SIGINT, signal.SIG_IGN)
     output = proc.communicate()
-    signal.signal(signal.SIGINT, signal.default_int_handler)
 
     stdout = output[0].splitlines()[-1]
     result = eval(stdout.decode())
@@ -63,7 +76,7 @@ def test_no_kill():
 
 
 if __name__ == '__main__':
-    # test_no_kill()
+    #test_no_kill()
     test_single_kill()
     # test_double_kill()
 
--- qcodes/tests/nointerupt.py
+++ qcodes/tests/nointerupt.py
@@ -6,22 +6,22 @@ import time
 
 def not_to_be_interrupted():
     output = []
-
+    signal.signal
     try:
         with DelayedKeyboardInterrupt():
 
-            print(f"sub process signal handler {signal.getsignal(signal.SIGINT)}")
-            print(f"sub pid = {os.getpid()}")
+            # print(f"sub process signal handler {signal.getsignal(signal.SIGINT)}")
+            # print(f"sub pid = {os.getpid()}")
             for i in range(20):
-                print(f"sub process signal handler {signal.getsignal(signal.SIGINT)}")
+                # print(f"sub process signal handler {signal.getsignal(signal.SIGINT)}")
                 time.sleep(0.1)
                 output.append(i)
-                print(i)
-                print(f"sub process signal handler {signal.getsignal(signal.SIGINT)}")
+                # print(i)
+                # print(f"sub process signal handler {signal.getsignal(signal.SIGINT)}")
             output.append("done_looping")
-            print("done_looping")
+            # print("done_looping")
         output.append('completed')
-        print("completed")
+        # print("completed")
     except KeyboardInterrupt:
         pass
         # print(f"at interrupt sub process signal handler {signal.getsignal(signal.SIGINT)}")
