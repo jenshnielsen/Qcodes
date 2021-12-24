730d0de3849024ca07bb5f63186de4a9e0d67957
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-12-24 09:51:43 +0100

    Logic for loading version at runtime

--- qcodes/version.py
+++ qcodes/version.py
@@ -1,3 +1,17 @@
-import qcodes
+import os
+from pathlib import Path
 
-__version__ = qcodes.__version__
+import versioningit
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+
+if True:
+    # if installed editable
+    pyprojectpath = Path(os.path.abspath(__file__)).parent.parent
+    print(pyprojectpath)
+    __version__ = versioningit.get_version(project_dir=pyprojectpath)
+else:
+    __version__ = version("qcodes")
