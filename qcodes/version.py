fe43d4c4370cbb4616cca322dcc355f85d22c5fb
Jens H. Nielsen <Jens.Nielsen@microsoft.com>
2021-12-24 09:58:07 +0100

    reorder version info logic

--- qcodes/version.py
+++ qcodes/version.py
@@ -1,17 +1,3 @@
-import os
-from pathlib import Path
+from qcodes.utils.installation_info import get_qcodes_version
 
-import versioningit
-
-try:
-    from importlib.metadata import version
-except ImportError:
-    from importlib_metadata import version
-
-if True:
-    # if installed editable
-    pyprojectpath = Path(os.path.abspath(__file__)).parent.parent
-    print(pyprojectpath)
-    __version__ = versioningit.get_version(project_dir=pyprojectpath)
-else:
-    __version__ = version("qcodes")
+__version__ = get_qcodes_version()
